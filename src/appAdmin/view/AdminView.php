192cd7e9e99fa676047be482fed2e48cb0d0ed52
Theo57320 <theoantolinig@gmail?com>
2021-11-04 11:39:33 +0100

    commandes

--- src/appAdmin/view/AdminView.php
+++ src/appAdmin/view/AdminView.php
@@ -118,9 +118,7 @@ class AdminView extends \mf\view\AbstractView {
         $router = new \mf\router\Router();
         $resultat="<div>";
         $resultat= $resultat."Commandes</div>";
-        $resultat =$this->data->produit;
-        return $resultat;
-    }
 
+    }
 
 }
